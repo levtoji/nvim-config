739d3e2bf8744d6bcd62a1aca1714107fa796375
Lev <lev.perschin@gmail.com>
2026-09-08 22:47:00 +0200

    Lazygit als schwebendes Terminal ergänzen (<leader>gl)
    Neogit bleibt Standard für den Alltag, Lazygit zusätzlich für
    Rebases/Merge-Konflikte, wo es die stärkere UI hat.

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -4,10 +4,28 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   cmd = { "ToggleTerm", "TermExec" },
-  keys = { [[<c-\>]] },
+  keys = { [[<c-\>]], "<leader>gl" },
   opts = {
     open_mapping = [[<c-\>]],
     direction = "float",
     float_opts = { border = "curved" },
   },
+  config = function(_, opts)
+    require("toggleterm").setup(opts)
+
+    -- Lazygit als schwebendes Terminal daneben: Neogit bleibt Standard,
+    -- Lazygit für Rebases/Merge-Konflikte, wo es die bessere UI hat.
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      hidden = true,
+      float_opts = { border = "curved" },
+    })
+
+    vim.keymap.set("n", "<leader>gl", function()
+      lazygit:toggle()
+    end, { desc = "Lazygit" })
+  end,
 }
