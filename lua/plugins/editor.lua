66dae6d78c3af5cafa3ad8a287b80726a494da7b
Lev <lev.perschin@gmail.com>
2026-09-08 23:18:13 +0200

    Comment.nvim entfernen - obsolet seit Neovim 0.10
    gc/gcc/gc-Textobjekt kommen nativ aus dem Core (vim._comment, :h gc-default),
    das Plugin lud zudem bisher eager bei jedem Start statt lazy. gco/gcO/gcA
    (Comment.nvim-exklusiv) fallen weg, dafür ein Dependency weniger.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,6 @@
+-- gc/gcc/gc-Textobjekt kommen seit Neovim 0.10 nativ aus dem Core
+-- (vim._comment, siehe :h gc-default) - kein Comment.nvim mehr nötig.
 return {
-  { "numToStr/Comment.nvim", opts = {} },
   { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
   { "kylechui/nvim-surround", event = "VeryLazy", opts = {} },
 }
