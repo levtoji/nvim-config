be9628d8cb352d72124101b09f1213adbe478114
Lev <lev.perschin@gmail.com>
2026-09-09 01:40:37 +0200

    NodeProjects und Spielereien als weitere Projekt-Roots ergänzen

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -7,6 +7,8 @@ return {
     projects = {
       "~/RiderProjects/*",
       "~/WebstormProjects/*",
+      "~/NodeProjects/*",
+      "~/Spielereien/*",
       "~/.config/*",
     },
     picker = {
